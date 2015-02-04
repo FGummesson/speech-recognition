f1b0e389ac09e6e35a16f150d8f88ddeef33bd4b
Filip Gummesson <filip.gummesson@gmail.com>
2015-02-04 11:51:16 +0100

    rewritten algorithm for cutting signals.

--- cut.m
+++ cut.m
@@ -5,47 +5,55 @@ function [ signal ] = cut( signal, b_length, overlap, threshold )
     
     row = length(y(1,:));
     
+    first = 0;
     for i = 1:row
         temp = y(:,i);
-        if norm(temp) < threshold
-            y(:,i) = zeros(b_length, 1);
-        end
-    end
-    signal = y(:);
-    
-    i = 1;
-    while i < length(signal)
-        if signal(i) ~= 0
+        if norm(temp) > threshold
+            first = (i - 4) * b_length;
             break;
         end
-        i = i +1;
     end
     
-    
-    j = length(signal);
-    
-    while j > i
-       if signal(j) ~= 0
-           break;
-       end
-       j = j -1;
+    last = row * b_length;
+    for i = fliplr(1:row)
+        temp = y(:,i);
+        if norm(temp) > threshold
+            last = (i + 2) * b_length;
+            break;
+        end
     end
     
-    signal = signal(i:j);
+    signal = y(:);
+    signal = signal(first:last);
+    
+    
+    
+    
+    
+    
+    
+    %%
 %     
-%     
-%     for i = 1:length(signal)
+%     i = 1;
+%     while i < length(signal)
 %         if signal(i) ~= 0
-%             signal = signal(i:length(signal));
 %             break;
 %         end
-%   
-%     for i = fliplr(1:length(signal))
-%         if signal(i) ~= 0
-%             signal = signal(i:length(signal));
-%             break;
-%         end
+%         i = i +1;
 %     end
+%     
+%     
+%     j = length(signal);
+%     
+%     while j > i
+%        if signal(j) ~= 0
+%            break;
+%        end
+%        j = j -1;
+%     end
+%     
+%     signal = signal(i:j);
+
     
 end
 
