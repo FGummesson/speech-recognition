c6a537f2e667cec3371a91039379d27f6c66b0b0
Filip Gummesson <filip.gummesson@gmail.com>
2015-02-04 11:19:55 +0100

    remove first and last part of a signal.

---
+++ split.m
@@ -0,0 +1,51 @@
+function [ signal ] = split( signal, b_length, overlap, threshold )
+% Cut out the important values of a speech signal.
+    
+    y = buffer(signal, b_length, overlap);
+    
+    row = length(y(1,:));
+    
+    for i = 1:row
+        temp = y(:,i);
+        if norm(temp) < threshold
+            y(:,i) = zeros(b_length, 1);
+        end
+    end
+    signal = y(:);
+    
+    i = 1;
+    while i < length(signal)
+        if signal(i) ~= 0
+            break;
+        end
+        i = i +1;
+    end
+    
+    
+    j = length(signal);
+    
+    while j > i
+       if signal(j) ~= 0
+           break;
+       end
+       j = j -1;
+    end
+    
+    signal = signal(i:j);
+%     
+%     
+%     for i = 1:length(signal)
+%         if signal(i) ~= 0
+%             signal = signal(i:length(signal));
+%             break;
+%         end
+%   
+%     for i = fliplr(1:length(signal))
+%         if signal(i) ~= 0
+%             signal = signal(i:length(signal));
+%             break;
+%         end
+%     end
+    
+end
+
