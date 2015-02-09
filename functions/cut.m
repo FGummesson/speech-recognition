52e5da23271082f9997a8793659e84f8977a553d
Filip Gummesson <filip.gummesson@gmail.com>
2015-02-09 13:18:52 +0100

    cut returns the norm of each block

--- functions/cut.m
+++ functions/cut.m
@@ -1,16 +1,22 @@
-function [ output ] = cut( input, b_length, overlap, threshold )
+function [ output, norms ] = cut( input, b_length, overlap, threshold )
 % Cut out the important values of a speech signal.
     % cut(signal, b_length, overlap, threshold)
     % b_length = block length
     % overlap = amount of overlap in samples
     % threshold = when to start listening. Around 1.
-
+    
     y = buffer(input, b_length, overlap);
     
-    row = length(y(1,:));
+    n_cols = length(y(1,:));
+    
+    norms = [];
+    for i = 1:n_cols
+        norms = [norms, norm(y(:,i))]; 
+    end
+    
     
     first = 0;
-    for i = 1:row
+    for i = 1:n_cols
         temp = y(:,i);
         if norm(temp) > threshold
             first = ((i -4) * b_length);
@@ -22,8 +28,8 @@ function [ output ] = cut( input, b_length, overlap, threshold )
         first = 1;
     end
     
-    last = row * b_length;
-    for i = fliplr(1:row)
+    last = n_cols * b_length;
+    for i = fliplr(1:n_cols)
         temp = y(:,i);
         if norm(temp) > threshold
             last = (i + 2) * b_length;
