58e1a7470f48ec776f3623a82433476b0ba61032
HannaER <hannaruner@gmail.com>
2015-02-09 14:17:02 +0100

    matlab
    fix

--- functions/create_features.m
+++ functions/create_features.m
@@ -11,17 +11,18 @@ function create_features(input, string, L, D, P, fs, sound)
 %   *fs* is the sample frequency the input signal was sampled with
 
 
-figure (1)
-subplot(221)
-plot(input)
-title('Original recording')
-xlabel('Samples');
-ylabel('Amplitude');
-
-figure (2)
-fft_(input, length(input), fs, 221)
 if sound == 1
     soundsc(input, fs)
+    
+    figure (1)
+    subplot(221)
+    plot(input)
+    title('Original recording')
+    xlabel('Samples');
+    ylabel('Amplitude');
+    
+    figure (2)
+    fft_(input, length(input), fs, 221)
 end
 
 
@@ -29,54 +30,54 @@ end
 % removing low frequency noise with filter(-s)
 input_0 = rm_noise(input);
 
-figure (1)
-subplot(222)
-plot(input_0)
-title('After high and low pass filtering')
-xlabel('Samples');
-ylabel('Amplitude');
-
-figure (2)
-fft_(input_0, length(input_0), fs, 222)
-
 if sound ==1
     pause(3)
     soundsc(input_0, fs);
+    figure (1)
+    subplot(222)
+    plot(input_0)
+    title('After high and low pass filtering')
+    xlabel('Samples');
+    ylabel('Amplitude');
+    
+    figure (2)
+    fft_(input_0, length(input_0), fs, 222)
 end
 
 % pre-emhasis filter with a = 0.95-98, function
 input_1 = pre_emph(input_0);
 
-figure (1)
-subplot(223)
-plot(input_1)
-title('After pre-emphasis')
-xlabel('Samples');
-ylabel('Amplitude');
-
-figure (2)
-fft_(input_1, length(input_1), fs, 223)
 if sound ==1
     pause(3)
     soundsc(input_1, fs);
+    figure (1)
+    subplot(223)
+    plot(input_1)
+    title('After pre-emphasis')
+    xlabel('Samples');
+    ylabel('Amplitude');
+    
+    figure (2)
+    fft_(input_1, length(input_1), fs, 223)
 end
 
 % Remove unecessary parts of signal at beginning and end
-threshold = 1;
-input_2 = cut(input_1, 100, 0,threshold);
+threshold = 0.12;
+input_2 = cut(input_1, 100, threshold);
 
-figure (1)
-subplot(224)
-plot(input_2)
-title('After cutting the signal')
-xlabel('Samples');
-ylabel('Amplitude');
 
-figure (2)
-fft_(input_2, length(input_2), fs, 224)
 if sound == 1
     pause(3)
     soundsc(input_2, fs);
+    figure (1)
+    subplot(224)
+    plot(input_2)
+    title('After cutting the signal')
+    xlabel('Samples');
+    ylabel('Amplitude');
+    
+    figure (2)
+    fft_(input_2, length(input_2), fs, 224)
 end
 
 % Add zeros such that there will be a multiple of L samples
@@ -92,7 +93,7 @@ input_4 = block_frame(input_2, L, D);
 input_5 = schur_algo(input_4, P);
 
 % Divide into subsets and calculate the mean of the coefficients
-M = 8; % number of subsets
+M = 12; % number of subsets
 
 features = create_subsets(input_5, M);
 
