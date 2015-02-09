3dd79b225b72385aaf9edecca671fea707eed3a7
HannaER <hannaruner@gmail.com>
2015-02-09 13:19:18 +0100

    matlab fix
    grejjat lite

--- functions/testrecognition.m
+++ functions/testrecognition.m
@@ -3,7 +3,7 @@
 clc;
 close all;
 clear all;
-
+load task5.mat
 
 % 'four', t1,x1, x5
 % 'eigth', t2, x2, x4
@@ -12,5 +12,11 @@ clear all;
 
 % Call the function 'matching' to calculate the error of the 
 % word-matching. Input is the feature matrix and data base
-load eigth.mat
-four1 = matching( , output)
+load four.mat
+load 41.mat
+four0 = matching(db, db)
+four1 = matching(features , db)
+load 42.mat
+four2 = matching(features, db)
+load 81.mat
+four3 = matching(features, db)
