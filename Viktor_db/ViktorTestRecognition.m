48be1f56006d609e8eda13fc8d2971d960056d38
VktorStevander <Viktor.Stevander@gmail.com>
2015-02-11 14:18:21 +0100

    viktor database and original signals

---
+++ Viktor_db/ViktorTestRecognition.m
@@ -0,0 +1,28 @@
+clc;
+close all;
+clear all;
+load task5.mat
+
+% Call the function 'matching' to calculate the error of the 
+% word-matching. Input is the feature matrix and data base
+
+load Right1
+right1 = db;
+
+load Right2
+right2 = db;
+
+load Right3
+right3 = db;
+
+load Right4
+right4 = db;
+
+load Right5
+right5 = db;
+
+r1_mot_r1 = matching(right1, right1)
+r1_mot_r2 = matching(right1, right2)
+r1_mot_r3 = matching(right1, right3)
+r1_mot_r4 = matching(right1, right4)
+r1_mot_r5 = matching(right1, right5)
