f6d3dfc49d2844add0f488d2578e093b9c8caeed
VktorStevander <Viktor.Stevander@gmail.com>
2015-02-13 14:01:53 +0100

    Nya värden
    Nya värden

--- Viktor_db/ViktorTestRecognition.m
+++ Viktor_db/ViktorTestRecognition.m
@@ -36,26 +36,32 @@ l4 = db;
 load Viktor_db/database/Left5
 l5 = db;
 
-r1_mot_r1 = matching(right1, right1)
-r1_mot_r2 = matching(right1, right2)
-r1_mot_r3 = matching(right1, right3)
-r1_mot_r4 = matching(right1, right4)
-r1_mot_r5 = matching(right1, right5)
+load Viktor_db/database/NotLR
+NotLR = db;
 
-total_error_right = r1_mot_r1 + r1_mot_r2 + ...
-    r1_mot_r3 + r1_mot_r4 + r1_mot_r5
+r1_mot_r1 = matching(right2, right1)
+r1_mot_r2 = matching(right2, right2)
+r1_mot_r3 = matching(right2, right3)
+r1_mot_r4 = matching(right2, right4)
+r1_mot_r5 = matching(right2, right5)
+
+r1_mot_l1 = matching(right2, l1)
+r1_mot_l2 = matching(right2, l2)
+r1_mot_l3 = matching(right2, l3)
+r1_mot_l4 = matching(right2, l4)
+r1_mot_l5 = matching(right2, l5)
+
+%total_error_right = r1_mot_r1 + r1_mot_r2 + ...
+%    r1_mot_r3 + r1_mot_r4 + r1_mot_r5
 X = [r1_mot_r1 r1_mot_r2 ...
     r1_mot_r3 r1_mot_r4 r1_mot_r5];
 aver_error_right = mean(X)
 
-r1_mot_l1 = matching(right1, l1)
-r1_mot_l2 = matching(right1, l2)
-r1_mot_l3 = matching(right1, l3)
-r1_mot_l4 = matching(right1, l4)
-r1_mot_l5 = matching(right1, l5)
-
-total_error_left = r1_mot_l1 + r1_mot_l2 + ...
-    r1_mot_l3 + r1_mot_l4 + r1_mot_l5
+%total_error_left = r1_mot_l1 + r1_mot_l2 + ...
+%    r1_mot_l3 + r1_mot_l4 + r1_mot_l5
 X = [r1_mot_l1 r1_mot_l2 ...
     r1_mot_l3 r1_mot_l4 r1_mot_l5];
 avr_error_left = mean(X)
+
+NotLR_mot_r2 = matching(right2, NotLR)
+NotLR_mot_l1 = matching(l1, NotLR)
--- Viktor_db/addingOriginalToDB.m
+++ Viktor_db/addingOriginalToDB.m
@@ -1,29 +1,32 @@
 load VTestRight1
-add_to_db(rec, 'Viktor_db/database/Right1', 140, 80, 8, 8000, 0.05, 0)
+add_to_db(rec, 'Viktor_db/database/Right1', 160, 80, 8, 8000, 0.07, 0)
 
 load VTestRight2
-add_to_db(rec, 'Viktor_db/database/Right2', 140, 80, 8, 8000, 0.05, 0)
+add_to_db(rec, 'Viktor_db/database/Right2', 160, 80, 8, 8000, 0.07, 0)
 
 load VTestRight3
-add_to_db(rec, 'Viktor_db/database/Right3', 140, 80, 8, 8000, 0.05, 0)
+add_to_db(rec, 'Viktor_db/database/Right3', 160, 80, 8, 8000, 0.07, 0)
 
 load VTestRight4
-add_to_db(rec, 'Viktor_db/database/Right4', 140, 80, 8, 8000, 0.05, 0)
+add_to_db(rec, 'Viktor_db/database/Right4', 160, 80, 8, 8000, 0.07, 0)
 
 load VTestRight5
-add_to_db(rec, 'Viktor_db/database/Right5', 140, 80, 8, 8000, 0.05, 0)
+add_to_db(rec, 'Viktor_db/database/Right5', 160, 80, 8, 8000, 0.07, 0)
 
 load VTestLeft1
-add_to_db(rec, 'Viktor_db/database/Left1', 140, 80, 8, 8000, 0.05, 0)
+add_to_db(rec, 'Viktor_db/database/Left1', 160, 80, 8, 8000, 0.07, 0)
 
 load VTestLeft2
-add_to_db(rec, 'Viktor_db/database/Left2', 160, 80, 8, 8000, 0.05, 0)
+add_to_db(rec, 'Viktor_db/database/Left2', 160, 80, 8, 8000, 0.07, 0)
 
 load VTestLeft3
-add_to_db(rec, 'Viktor_db/database/Left3', 160, 80, 8, 8000, 0.05, 0)
+add_to_db(rec, 'Viktor_db/database/Left3', 160, 80, 8, 8000, 0.07, 0)
 
 load VTestLeft4
-add_to_db(rec, 'Viktor_db/database/Left4', 160, 80, 8, 8000, 0.05, 0)
+add_to_db(rec, 'Viktor_db/database/Left4', 160, 80, 8, 8000, 0.07, 0)
 
 load VTestLeft5
-add_to_db(rec, 'Viktor_db/database/Left5', 160, 80, 8, 8000, 0.05, 0)
+add_to_db(rec, 'Viktor_db/database/Left5', 160, 80, 8, 8000, 0.07, 0)
+
+load NotLR
+add_to_db(rec, 'Viktor_db/database/NotLR', 160, 80, 8, 8000, 0.07, 0)
