df125170fa1cb66f983584b8a14d8e7f0df0df03
HannaER <hannaruner@gmail.com>
2015-03-05 16:56:36 +0100

    toFile
    komplett kodgenerator

--- functions/toFile.m
+++ functions/toFile.m
@@ -1,26 +1,154 @@
-%function toFile(db)
-    
-    
-    formatSpec = 'b%d.reflect[%d] = %f;\n';
-    fid = fopen('test.txt', 'w');
-    
-    fprintf(fid, 'block_t b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12; \n');
-   
-    
-    for col = 1:12
-        for row = 1:8
-            fprintf(fid, formatSpec,col,row,db(row,col));
-            
-        end
-            fprintf(fid, 'energy = -1\n');
-            fprintf(fid, '\n');
-    end
-    
-        fprintf(fid, 'version_t version1; \n');
-        formatSpec = 'version1.version[%d] = b%d;\n';
-        for i = 1:12
-            fprintf(fid, formatSpec, i-1, i);
-        end
-    fclose(fid);
+clc;
+clear all,
+close all;
 
-%end
+N_WORDS = 2;
+N_VERSIONS = 2;
+SUBSET_LENGTH = 12;
+N_REFLEC = 9;
+
+current_database =[];
+
+%HÖGER VERSIONS
+load r1.mat;
+s.name = 'right';
+s.reflec = db;
+current_database = [current_database s];
+load r2.mat;
+s.name = 'right';
+s.reflec = db;
+current_database = [current_database s];
+
+%VÄNSTER VERSIONS
+load l1.mat;
+s.name = 'left';
+s.reflec = db;
+current_database = [current_database s];
+load l2.mat;
+s.name = 'left';
+s.reflec = db;
+current_database = [current_database s];
+
+
+
+tempstr = '\tblock_t ';
+formatSpec = 'b%d.reflect[%d] = %f;\n';
+fid = fopen('test.txt', 'w');
+fprintf(fid, '#include "load_db.h" \n\nvoid load_db(db_t* current_db){\n\n');
+% strcat för b1 b2 .. strängen
+for i= 1:SUBSET_LENGTH
+   tempstr = strcat(tempstr, ' b', num2str(i-1));
+   if (i < SUBSET_LENGTH )
+    tempstr = strcat(tempstr, ', ');
+   end
+   
+end
+tempstr = strcat(tempstr, '; \n');
+fprintf(fid, tempstr);
+fprintf(fid, '\tversion_t version; \n');
+fprintf(fid, '\tword_t word; \n');
+fprintf(fid, '\tdb_t db; \n');
+
+counter = 0;
+
+for i = 1:N_WORDS
+    for j = 1:N_VERSIONS   
+        counter = counter + 1;
+        for col = 1:SUBSET_LENGTH
+            for row = 1:N_REFLEC
+                formatSpec = '\tb%d.reflect[%d] = %f;\n';
+                fprintf(fid, formatSpec,col-1,row -1,current_database(counter).reflec(row, col));                
+            end            
+            fprintf(fid, '\tb%d.energy = -1;\n', col-1); % hämta den riktiga energin?
+            fprintf(fid, '\n');
+            formatSpec = '\tversion.subset[%d] = b%d;\n \n';
+            fprintf(fid, formatSpec, col -1 ,col-1  );
+        end
+        formatSpec = '\tword.versions[%d] = version;\n \n';
+        fprintf(fid, formatSpec, j-1); 
+
+    end
+        formatSpec = '\tword.name = "%s";\n \n';
+        fprintf(fid, formatSpec, current_database(counter).name);
+        formatSpec = '\tdb.words[%d] = word;\n \n';
+        fprintf(fid, formatSpec, i -1);
+
+end
+
+
+fprintf(fid, '\t*current_db = db; \n');
+fprintf(fid, '\treturn; \n');
+fprintf(fid, '} \n');
+fclose(fid);
+
+%%
+
+
+
+% /*
+% 	block_t b1, b2, b3 ;()
+% 	b1.reflect[0] = 1;
+% 	b1.reflect[1] = 2;
+% 	b1.energy = -1;
+% 	
+% 	b2.reflect[0] = 2;
+% 	b2.reflect[1] = 3;
+% 	b2.energy = -1;
+% 
+% 	b3.reflect[0] = 3;
+% 	b3.reflect[1] = 4;
+% 	b3.energy = -1;
+% 
+% 
+% 	version_t version1;
+% 
+% 	version1.subset[0] = b1;
+% 	version1.subset[1] = b2;
+% 	version1.subset[2] = b3;
+% 
+% 	
+% 	b1.reflect[0] = 2;
+% 	b1.reflect[1] = 3;
+% 	b1.energy = -1;
+% 	
+% 	b2.reflect[0] = 3;
+% 	b2.reflect[1] = 4;
+% 	b2.energy = -1;
+% 
+% 	b3.reflect[0] = 4;
+% 	b3.reflect[1] = 5;
+% 	b3.energy = -1;
+% 
+% 	version_t version2;
+% 
+% 	version2.subset[0] = b1;
+% 	version2.subset[1] = b2;
+% 	version2.subset[2] = b3;
+% 	
+% 	b1.reflect[0] = 3;
+% 	b1.reflect[1] = 4;
+% 	b1.energy = -1;
+% 	
+% 	b2.reflect[0] = 4;
+% 	b2.reflect[1] = 5;
+% 	b2.energy = -1;
+% 
+% 	b3.reflect[0] = 5;
+% 	b3.reflect[1] = 6;
+% 	b3.energy = -1;
+% 	
+% 	version_t version3;
+% 
+% 	version3.subset[0] = b1;
+% 	version3.subset[1] = b2;
+% 	version3.subset[2] = b3;
+% 
+% 	word_t word1;
+% 
+% 	word1.versions[0] = version1;
+% 	word1.versions[1] = version2;
+% 	word1.versions[2] = version3;
+% 	word1.name = "Test: word1";
+% 	db_t database;
+% 	database.words[0] = word1;
+% 	*current_db = database;
